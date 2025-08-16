34cc0bd092812b90b497071469106acbdd85934c
saketbishnu <saketbsn@gmail.com>
2025-08-17 03:01:15 +0530

    updated doctor list

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -88,7 +88,7 @@ export const specialityData = [
 export const doctors = [
     {
         _id: 'doc1',
-        name: 'Dr. Richard James',
+        name: 'Dr. Parimal Pandit',
         image: doc1,
         speciality: 'General physician',
         degree: 'MBBS',
